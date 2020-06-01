8c249b96b59eb44879aa874671f884caecfab92a
Stephen Toms <steve@stephens-mbp.myfiosgateway.com>
2020-06-01 19:15:18 -0400

    added missing files

---
+++ components/PhaserContainer.vue
@@ -0,0 +1,94 @@
+<template>
+  <div :id="phaserContainer" />
+</template>
+
+<script>
+export default {
+  name: 'PhaserContainer',
+
+  props: {
+    width: {
+      type: Number,
+      default: 800
+    },
+    height: {
+      type: Number,
+      default: 600
+    },
+    pageContainer: {
+      type: String,
+      default: 'container'
+    },
+    phaserContainer: {
+      type: String,
+      default: 'phaser-container'
+    },
+    fixedSize: {
+      type: Boolean,
+      default: false
+    },
+    game: {
+      default() {
+        return {}
+      }
+    }
+  },
+  computed: {
+    componentPayload() {
+      switch (this.exampleComponentProps.type) {
+        // if the page is loading a Phaser game
+        case 'phaser': {
+          this.$store.commit(
+            'initializePhaser',
+            this.exampleComponentProps.loaderKey
+          )
+          return this.$phaserLoader.loadGame(
+            this.exampleComponentProps.loaderKey
+          )
+        }
+        default:
+          return {}
+      }
+    }
+  },
+
+  destroyed() {
+    this.gameObject.destroy()
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      // get page's main container (for sizing)
+      const pageContainer = document.getElementsByClassName(
+        this.pageContainer
+      )[0]
+
+      // launch game with resizing
+      if (this.fixedSize === false) {
+        this.gameObject = this.game.launch({
+          width:
+            pageContainer.clientWidth < this.width
+              ? pageContainer.clientWidth
+              : this.width,
+          height:
+            pageContainer.clientHeight < this.height
+              ? pageContainer.clientHeight
+              : pageContainer.clientWidth * 0.7 < this.height
+              ? pageContainer.clientWidth * 0.7
+              : this.height,
+          parent: this.phaserContainer,
+          store: this.$store ? this.$store : null
+        })
+        // launch game without resizing
+      } else {
+        this.gameObject = this.game.launch({
+          width: this.width,
+          height: this.height,
+          parent: this.phaserContainer,
+          store: this.$store ? this.$store : null
+        })
+      }
+    })
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,88 @@
+module.exports = {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // exclude Phaser image assets from url-loader
+      config.module.rules.forEach((rule) => {
+        if (rule.test.toString().includes('png')) {
+          rule.exclude = /phaser/
+        }
+      })
+      // load Phaser image assets
+      config.module.rules.push({
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        loader: 'file-loader',
+        options: {
+          include: /phaser/,
+          name: '[path][name].[ext]'
+        }
+      })
+      // load Phaser audio assets
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      })
+    }
+  }
+}
